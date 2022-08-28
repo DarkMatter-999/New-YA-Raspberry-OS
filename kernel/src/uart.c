fee0576d1ff1301363f055f171d32373c3c4ddbe
DarkMatter-999 <darkmatter999official@gmail.com>
2022-08-28 22:15:18 +0530

    Added MailBoxes

--- kernel/src/uart.c
+++ kernel/src/uart.c
@@ -1,7 +1,7 @@
 #include "uart.h"
 #include "gpio.h"
 
-/* Auxilary mini UART registers */
+// Auxilary mini UART registers
 #define AUX_ENABLE      ((volatile unsigned int*)(MMIO_BASE+0x00215004))
 #define AUX_MU_IO       ((volatile unsigned int*)(MMIO_BASE+0x00215040))
 #define AUX_MU_IER      ((volatile unsigned int*)(MMIO_BASE+0x00215044))
@@ -15,9 +15,7 @@
 #define AUX_MU_STAT     ((volatile unsigned int*)(MMIO_BASE+0x00215064))
 #define AUX_MU_BAUD     ((volatile unsigned int*)(MMIO_BASE+0x00215068))
 
-/**
- * Set baud rate and characteristics (115200 8N1) and map to GPIO
- */
+// Set baud rate and characteristics (115200 8N1) and map to GPIO
 void uart_init()
 {
     register unsigned int r;
@@ -43,9 +41,7 @@ void uart_init()
     *AUX_MU_CNTL = 3;      // enable Tx, Rx
 }
 
-/**
- * Send a character
- */
+// Send a character
 void uart_send(unsigned int c) {
     /* wait until we can send */
     do{asm volatile("nop");}while(!(*AUX_MU_LSR&0x20));
@@ -53,9 +49,7 @@ void uart_send(unsigned int c) {
     *AUX_MU_IO=c;
 }
 
-/**
- * Receive a character
- */
+// Receive a character
 char uart_getc() {
     char r;
     /* wait until something is in the buffer */
@@ -66,14 +60,25 @@ char uart_getc() {
     return r=='\r'?'\n':r;
 }
 
-/**
- * Display a string
- */
+// Display a string
 void uart_puts(char *s) {
     while(*s) {
         /* convert newline to carriage return + newline */
         if(*s=='\n')
             uart_send('\r');
         uart_send(*s++);
+    }
+}
+
+// Display a binary value in hexadecimal
+void uart_hex(unsigned int d) {
+    unsigned int n;
+    int c;
+    for(c=28;c>=0;c-=4) {
+        // get highest tetrad
+        n=(d>>c)&0xF;
+        // 0-9 => '0'-'9', 10-15 => 'A'-'F'
+        n+=n>9?0x37:0x30;
+        uart_send(n);
     }
 }
